d13888c26a39d5efd85a86d891cb9f19f586ca4c
BabyBoo <trieungoc2503@gmail.com>
2023-10-10 14:56:14 +0700

    a

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ renderItem=(products)=>{
     for(i=0; i<8; i++){
     output += `
       <div class="item">
-         <a href="chi-tiet.html#`+products[i].id+`" class="item-img"><img src="`+products[i].image+`"/></a>
+         <a href="../chi-tiet.html#`+products[i].id+`" class="item-img"><img src="`+products[i].image+`"/></a>
          <p class="item-name">`+products[i].title+`</p> 
          <p class="book-author">`+products[i].author+`</p>             
          <p class="item-price">`+products[i].price+'.000đ'+`</p>
@@ -35,7 +35,7 @@ window.addEventListener('load', ()=>{
                             <h6>${title}</h6>           
                             <p>${price}.000đ</p>
                          </div>`;
-        card.href = 'chi-tiet.html' + '#' + id
+        card.href = '../chi-tiet.html' + '#' + id
         box.appendChild(card);
     })
 })
